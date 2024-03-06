7757e49fc83a71bfc9835f71d6a4ff8c176cf1a9
Socvest <82335390+Socvest@users.noreply.github.com>
2024-03-06 20:58:29 +0000

    Update LocalStorageComponent.tsx

--- streamlit_local_storage/frontend/src/LocalStorageComponent.tsx
+++ streamlit_local_storage/frontend/src/LocalStorageComponent.tsx
@@ -7,38 +7,44 @@ import {
 import React, { useEffect, useMemo, useState } from "react"
 import useLocalStorageState from 'use-local-storage-state'
 
-const LocalStorageComponent:React.FC<ComponentProps> = (props:any) => {
+const LocalStorageComponent: React.FC<ComponentProps> = (props: any) => {
 
-  const { args } = props 
-  const method:any = args["method"]
-  const itemKey:any = args["itemKey"]
-  const itemValue:any = args["itemValue"]
-  const localStrageOptions:any = args["localStrageOptions"] || { defaultValue: null }
-  const [todos, setTodos, {removeItem, isPersistent}] = useLocalStorageState<any>(itemKey, localStrageOptions)
+  const { args } = props
+  const method: any = args["method"]
+  const itemKey: any = args["itemKey"]
+  const itemValue: any = args["itemValue"]
+  const localStorageOptions: any = args["localStorageOptions"] || { defaultValue: null }
+  const [localStorageItems, setLocalStorageItems, { removeItem, isPersistent }] = useLocalStorageState<any>(itemKey, localStorageOptions)
 
-  const setItemF = (itemKey:any, itemValue:any) => {
-    let toSave = {item:itemKey, value:itemValue}
-    setTodos(toSave)
-  }  
+  const setItemF = (itemKey: any, itemValue: any) => {
+
+    let toSave = { [itemKey]: itemValue }
+    setLocalStorageItems(toSave)
+  }
 
   const deleteAll = () => {
     localStorage.clear();
     return true
   }
 
+  const eraseItem = (itemKey: any) => {
+    localStorage.removeItem(itemKey)
+  }
+
   const getAll = () => {
-    const toSendToStreamlit = {...localStorage}
+    const toSendToStreamlit = { ...localStorage }
+
     return toSendToStreamlit
   }
 
   useEffect(() => {
-    
-    switch(method){
+
+    switch (method) {
       case "getItem":
 
-        Streamlit.setComponentValue({"storage":todos, "dataPersist":isPersistent})
         Streamlit.setComponentReady()
-       
+        Streamlit.setComponentValue(localStorageItems)
+
         break
       case "setItem":
 
@@ -48,15 +54,21 @@ const LocalStorageComponent:React.FC<ComponentProps> = (props:any) => {
 
         removeItem()
         break
-      
+
+      case "eraseItem":
+
+        eraseItem(itemKey)
+        break
+
       case "deleteAll":
         deleteAll()
         break
-      
+
       case "getAll":
         const allStored = getAll()
-        Streamlit.setComponentValue(allStored)
         Streamlit.setComponentReady()
+        Streamlit.setComponentValue(allStored)
+
         break
 
       default:
@@ -64,15 +76,13 @@ const LocalStorageComponent:React.FC<ComponentProps> = (props:any) => {
     }
 
 
-  }, [method, setTodos, todos])
-  
-  
-  return ( 
-    <div style={{display:"none"}} >
-    </div>     
+  }, [method, setLocalStorageItems, localStorageItems])
+
+
+  return (
+    <div style={{ display: "none" }} >
+    </div>
   )
 }
 
 export default withStreamlitConnection(LocalStorageComponent)
-
-
